975472a91d1bbf9f5b933246b4cfd79ac7968474
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-08 16:48:54 +0300

    signcolumn width = 2

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -48,7 +48,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = 'yes:2'
 
 -- Decrease update time
 vim.o.updatetime = 250
