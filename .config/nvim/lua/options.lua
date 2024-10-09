657bd29a8f3a28bb89be72b9f54d44abc26cde05
Danila Slesarev <blackdeer1524@yandex.ru>
2024-10-09 18:26:45 +0300

    feat(nvim): colorscheme minor

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -95,59 +95,43 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 })
 
-vim.fn.sign_define(
-	"LspDiagnosticsSignError",
-	{ texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignWarning",
-	{ texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignHint",
-	{ texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignInformation",
-	{ texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation" }
-)
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	callback = function(ev)
-		local blend = function(d, a, c1, c2)
-			local a2 = d - a
-
-			local get_color_part = function(c, shift)
-				return bit.rshift(c, shift) % 0x100
-			end
-
-			local r = math.floor((a * get_color_part(c1, 16) + a2 * get_color_part(c2, 16)) / d)
-			local g = math.floor((a * get_color_part(c1, 8) + a2 * get_color_part(c2, 8)) / d)
-			local b = math.floor((a * get_color_part(c1, 0) + a2 * get_color_part(c2, 0)) / d)
-			return r * 0x10000 + g * 0x100 + b * 0x1
-		end
-
-		local pmenu_hl = vim.api.nvim_get_hl(0, { name = "PmenuSel" })
-		local background_color = blend(2, 1, pmenu_hl.bg, 0)
-		vim.api.nvim_set_hl(0, "PmenuSel", { fg = "NONE", bg = background_color })
-
-		local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-		local err_background_color = blend(30, 23, normal_hl.bg, 0xE86671)
-		local warning_background_color = blend(30, 23, normal_hl.bg, 0xFA973A)
-		local hint_background_color = blend(30, 23, normal_hl.bg, 0x17BE3B)
-		local info_background_color = blend(30, 23, normal_hl.bg, 0x3770E0)
-
-		vim.api.nvim_set_hl(0, "ErrorMsgLine", { bg = err_background_color, bold = true })
-		vim.api.nvim_set_hl(0, "WarningMsgLine", { bg = warning_background_color, bold = true })
-		vim.api.nvim_set_hl(0, "HintMsgLine", { bg = hint_background_color, bold = true })
-		vim.api.nvim_set_hl(0, "InfoMsgLine", { bg = info_background_color, bold = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "Makefile" },
+	callback = function(_)
+		vim.opt_local.expandtab = false
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = { "Makefile" },
-	callback = function(ev)
-		vim.opt_local.expandtab = false
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+	callback = function(_)
+		-- returns (a * c1 + (d - a) * c2) / d
+		local function blend(d, a, c1, c2)
+			assert(d >= a, "color blending assertion error")
+
+			local function get_color_part(c, shift)
+				return bit.rshift(c, shift) % 0x100
+			end
+
+			local r = math.floor((a * get_color_part(c1, 16) + (d - a) * get_color_part(c2, 16)) / d)
+			local g = math.floor((a * get_color_part(c1, 8) + (d - a) * get_color_part(c2, 8)) / d)
+			local b = math.floor((a * get_color_part(c1, 0) + (d - a) * get_color_part(c2, 0)) / d)
+			return bit.lshift(r, 16) + bit.lshift(g, 8) + b
+		end
+
+		local pmenu_hl = vim.api.nvim_get_hl(0, { name = "PmenuSel" })
+		local pmenu_sel_bg = blend(2, 1, pmenu_hl.bg, 0)
+		vim.api.nvim_set_hl(0, "PmenuSel", { fg = "NONE", bg = pmenu_sel_bg })
+
+		local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+		local erro_bg = blend(30, 23, normal_hl.bg, 0xE86671)
+		local warn_bg = blend(30, 23, normal_hl.bg, 0xFA973A)
+		local hint_bg = blend(30, 23, normal_hl.bg, 0x17BE3B)
+		local info_bg = blend(30, 23, normal_hl.bg, 0x3770E0)
+
+		vim.api.nvim_set_hl(0, "ErrorMsgLine", { bg = erro_bg, bold = true })
+		vim.api.nvim_set_hl(0, "WarningMsgLine", { bg = warn_bg, bold = true })
+		vim.api.nvim_set_hl(0, "HintMsgLine", { bg = hint_bg, bold = true })
+		vim.api.nvim_set_hl(0, "InfoMsgLine", { bg = info_bg, bold = true })
 	end,
 })
 
@@ -156,24 +140,24 @@ vim.diagnostic.config({
 		text = {
 			[vim.diagnostic.severity.ERROR] = "",
 			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.HINT] = "",
 			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
 		},
 		linehl = {
 			[vim.diagnostic.severity.ERROR] = "ErrorMsgLine",
 			[vim.diagnostic.severity.WARN] = "WarningMsgLine",
-			[vim.diagnostic.severity.HINT] = "HintMsgLine",
 			[vim.diagnostic.severity.INFO] = "InfoMsgLine",
+			[vim.diagnostic.severity.HINT] = "HintMsgLine",
 		},
 		numhl = {
-			[vim.diagnostic.severity.WARN] = "WarningMsg",
 			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
 		},
 	},
 	severity_sort = true,
 	update_in_insert = false,
 	virtual_text = true,
-	virtual_lines = false,
+	virtual_lines = false, -- see ErichDonGubler/lsp_lines.nvim
 	-- virtual_text = {
 	--   severity = vim.diagnostic.severity.WARN,
 	--   severity_sort = true,
