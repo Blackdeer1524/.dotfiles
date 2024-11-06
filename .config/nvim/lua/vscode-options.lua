121a03e9f607fb9b7918de0cc3af79a2e739971b
Danila Slesarev <blackdeer1524@yandex.ru>
2024-11-06 17:23:43 +0300

    panel minmax

--- .config/nvim/lua/vscode-options.lua
+++ .config/nvim/lua/vscode-options.lua
@@ -17,7 +17,7 @@ nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 nmap("<leader>cl", vim.lsp.codelens.run, "select [C]ode[L]ens")
 
 nmap("gd", "<cmd>lua require('vscode').action('editor.action.revealDefinition')<CR>", "[G]oto [D]efinition")
-nmap("gP", "<cmd>lua require('vscode').action('editor.action.peekDefinition')<CR>", "[G]oto [D]efinition")
+nmap("gp", "<cmd>lua require('vscode').action('editor.action.peekDefinition')<CR>", "[G]oto [D]efinition")
 
 nmap("gr", "<cmd>lua require('vscode').action('editor.action.goToReferences')<CR>", "[G]oto [R]eferences")
 nmap("gI", "<cmd>lua require('vscode').action('editor.action.goToImplementation')<CR>", "[G]oto [I]mplementation")
@@ -30,6 +30,7 @@ nmap("<leader>D", "<cmd>lua require('vscode').action('editor.action.goToTypeDefi
 nmap("<leader>K", vim.lsp.buf.signature_help, "Signature Documentation")
 
 nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+nmap("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [A]dd Folder")
 nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
 nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
 nmap(
