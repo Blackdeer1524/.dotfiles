86e5f797c7ca5285515dd1071b8d7b51031676d5
Danila Slesarev <blackdeer1524@yandex.ru>
2025-06-02 15:38:19 +0300

    added go test keymaps

--- .config/nvim/lua/vscode-conf/tests.lua
+++ .config/nvim/lua/vscode-conf/tests.lua
@@ -33,3 +33,32 @@ end)
 vim.keymap.set({ "n" }, "<leader>no", function()
 	require("vscode").action("testing.showMostRecentOutput")
 end)
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "go",
+	callback = function()
+		vim.keymap.set({ "n" }, "<leader>ndc", function()
+			require("vscode").action("go.debug.cursor")
+		end, { buffer = true })
+
+		vim.keymap.set({ "n" }, "<leader>ndl", function()
+			require("vscode").action("go.debug.previous")
+		end, { buffer = true })
+
+		vim.keymap.set({ "n" }, "<leader>nrl", function()
+			require("vscode").action("go.test.previous")
+		end, { buffer = true })
+
+		vim.keymap.set({ "n" }, "<leader>nrp", function()
+			require("vscode").action("go.test.package")
+		end, { buffer = true })
+
+		vim.keymap.set({ "n" }, "<leader>nrc", function()
+			require("vscode").action("go.test.cursor")
+		end, { buffer = true })
+
+		vim.keymap.set({ "n" }, "<leader>nrf", function()
+			require("vscode").action("go.test.file")
+		end, { buffer = true })
+	end,
+})
